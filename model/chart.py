7b09ffd2645ffe067668366c1542db3ecec242ec
vacanteye <vavanteye@hotmail.com>
2020-06-15 01:26:16 +0900

    chart modifed

--- model/chart.py
+++ model/chart.py
@@ -1,35 +1,30 @@
 import sqlite3, datetime
-from matplotlib import dates, ticker
-import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec`
-from matplotlib.finance import candlestick_ohlc
+import mplfinance as mpf
+import pandas as pd
 
-#Config Plot
-fig = plt.figure(figsize = (8,5))
-fig.set_facecolor('w')
-gs = gridspec.GridSpec(2, 1, height_ratios=[3,1])
+#Read Data
+con = sqlite3.connect('chart.db')
+df = pd.read_sql_query('SELECT * FROM chart_min_005930 ORDER BY date', con)
+print(df)
+con.close()
+
+#Refine data for charting
+df['date'] = pd.to_datetime(df['date'], format='%Y%m%d%H%M%S')
+df.rename(columns={"date":"Date", "open":"Open", "high":"High", "low":"Low", "close":"Close", "volume":"Volume"}, inplace=True)
+df = df.drop('amount',axis=1)
 
 
-axes = []
-axes.append(plt.subplot(gs[0])
-axes.append(plt.subplot(gs[1], sharex=axes[0]))
-axes[0].get_xaxis().set_visible(False)
+#Prepare Chart
+df.set_index('Date', inplace=True)
+df.shape
+df.head(3)
+df.tail(3)
 
-dateformat = '%Y%m%d%H%M%s'
-mpl.style.use('default')
+print(df)
+print(df.dtypes)
 
-conn = sqlite3.connect('chart.db')
-cur = conn.cursor()
-cur.execute('SELECT * FROM chart_min_005930')
-rows = cur.fetchall()
-for row in rows:
-    x = datetime.datetime.strptime(row[0], dateformat)
-    o = int(row[1])
-    h = int(row[2])
-    l = int(row[3])
-    c = int(row[4])
-    v = int(row[5])
-    a = int(row[6])
-    cdata = x, o, h, l, c, v
-    ohlc_data.append(cdata)
-conn.close()
+df = df.loc['2020-06-04':]
+mpf.plot(df, type='candle')
+
+
+con.close()
